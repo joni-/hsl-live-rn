d99fbbe2fb18c9518f304d4aae9d75374f7f2c77
Joni Nevalainen <joni.nevalainen@gofore.com>
2017-06-30 18:00:23 +0300

    Add map in the home screen

--- navigation/Router.js
+++ navigation/Router.js
@@ -1,12 +1,13 @@
 import { createRouter } from '@expo/ex-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
+import MapScreen from '../screens/MapScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import RootNavigation from './RootNavigation';
 
 export default createRouter(() => ({
-  home: () => HomeScreen,
+  home: () => MapScreen,
   links: () => LinksScreen,
   settings: () => SettingsScreen,
   rootNavigation: () => RootNavigation,
---
+++ screens/MapScreen.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MapView } from 'expo';
+
+export default class MapScreen extends React.Component {
+  render() {
+    return <MapView style={{ flex: 1 }} />;
+  }
+}
